9a5615c9264fea13eb2e7ea083f248548894a1fe
maxxykrane <adebowalemathew@gmail.com>
2024-10-22 17:33:40 +0100

    Add files via upload

---
+++ love.js
@@ -0,0 +1,7 @@
+alert ("Would you like to calculate love");
+prompt ("what is your name?");
+prompt("what is your lovers name?");
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your love score is " + loveScore + '%')
+
